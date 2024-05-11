bdb9bb637d08817b4094ad6efd06bd24bb1d3a6b
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-05-12 00:00:17 +0530

    Create checkCase.cpp

---
+++ Week2_Conditional&Loops/Check Case/checkCase.cpp
@@ -0,0 +1,63 @@
+/*
+CHECK CASE
+
+Problem statement
+Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
+
+
+
+1, if the character is an uppercase alphabet (A - Z).
+0, if the character is a lowercase alphabet (a - z).
+-1, if the character is not an alphabet.
+
+
+Example:
+Input: The character is 'a'.
+
+Output: 0
+
+Explanation: The input character is lowercase, so our answer is 0.
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1 :
+v
+
+
+Sample Output 1 :
+0
+
+
+Explanation of Sample Input 1:
+The input character is lowercase, so our answer is 0.
+
+
+Sample Input 2 :
+V
+
+
+Sample Output 2 :
+1
+
+*/
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	char char1;
+
+    cin>> char1;
+    if(char1>='A' && char1<='Z') {
+        cout<< "1" <<endl;
+    }
+
+    else if (char1>='a' && char1<='z') {
+        cout<< "0" <<endl;
+    }
+
+    else {
+        cout<< "-1";
+    }
+}
