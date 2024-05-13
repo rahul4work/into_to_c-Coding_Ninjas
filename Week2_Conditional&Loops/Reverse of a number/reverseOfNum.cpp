27f5c232aadd8474a4486ae3e21850d97c6e436d
Rahul Kumar <116094312+rahul4work@users.noreply.github.com>
2024-05-13 23:19:04 +0530

    Create reverseOfNum.cpp

---
+++ Week2_Conditional&Loops/Reverse of a number/reverseOfNum.cpp
@@ -0,0 +1,54 @@
+/*
+REVERSE OF A NUMBER
+
+Problem statement
+Write a program to generate the reverse of a given number N. Print 
+the corresponding reverse number.
+
+Note : If a number has trailing zeros, then its reverse will not 
+include them. For e.g., reverse of 10400 will be 401 instead of 00401.
+
+
+Constraints:
+0 <= N < 10^8
+
+
+--------------------------------------------------------------------
+Sample Input 1 :
+1234
+Sample Output 1 :
+4321
+--------------------------------------------------------------------
+
+
+--------------------------------------------------------------------
+Sample Input 2 :
+1980
+Sample Output 2 :
+891
+--------------------------------------------------------------------
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int N;
+    cin >> N;
+    int revNum = 0;
+
+    while (N > 0) {
+        int digit = N % 10;
+        revNum = revNum * 10 + digit;
+        N /= 10;
+    }
+    cout << revNum << endl;
+
+    return 0;
+	
+}
+
+
+
